1d9e49548d7feee64888b323f23f351fb7bb5938
cSuk1 <652240843@qq.com>
2023-12-04 00:01:57 +0800

    20231204

---
+++ chap3/fd.c
@@ -0,0 +1,73 @@
+/*******************************************************
+ * @file fd.c
+ * @author Andromeda (ech0uname@qq.com)
+ * @brief 文件描述符示例程序
+ * @version 0.1
+ * @date 2023-12-03
+ *
+ *
+ *******************************************************/
+
+#include "header.h"
+
+int main(int argc, char const *argv[])
+{
+    // 创建一个文件
+    int fd;
+    if ((fd = open("./test", O_RDWR | O_CREAT | O_TRUNC, 0666)) == -1)
+    {
+        perror("fail to create");
+    }
+    // 写入内容
+    char buff[BUFFER_SIZE];
+    bzero(buff, BUFFER_SIZE);
+    strcpy(buff, "hello world!");
+    write(fd, buff, strlen(buff));
+    close(fd);
+    // 读取内容
+    fd = open("./test", O_RDONLY);
+    bzero(buff, BUFFER_SIZE);
+    read(fd, buff, BUFFER_SIZE);
+    printf("%s\n", buff);
+    bzero(buff, BUFFER_SIZE);
+    close(fd);
+
+    // openat函数
+    fd = open("../chap1", O_RDONLY);
+    // fd参数指定path在文件系统中的开始位置
+    int fd1 = openat(fd, "./test", O_RDONLY);
+    bzero(buff, BUFFER_SIZE);
+    read(fd1, buff, BUFFER_SIZE);
+    printf("%s\n", buff);
+    close(fd1);
+    close(fd);
+
+    // 如果openat的第二个参数为绝对路径，则fd参数被忽略
+    fd = open("./", O_RDONLY);
+    fd1 = openat(fd, "/home/tsuk1/caixing/Advanced-Programming-in-the-Unix-Environment/chap1/test", O_RDONLY);
+    read(fd1, buff, BUFFER_SIZE);
+    printf("%s\n", buff);
+    bzero(buff, BUFFER_SIZE);
+    close(fd1);
+    close(fd);
+
+    // lseek函数
+    fd = open("./test", O_RDONLY);
+    bzero(buff, BUFFER_SIZE);
+    // 偏移量为2
+    off_t offset = 2;
+    // 设置fd偏移量
+    /**
+     * 第三个参数
+     * SEEK_SET	0	Seek from beginning of file.
+     * SEEK_CUR	1	Seek from current position.
+     * SEEK_END 2   Seek from end of file.
+     */
+    lseek(fd, offset, SEEK_CUR);
+    read(fd, buff, BUFFER_SIZE);
+    printf("%s\n", buff);
+    bzero(buff, BUFFER_SIZE);
+    close(fd);
+
+    return 0;
+}
---
+++ chap3/header.h
@@ -0,0 +1,17 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+#endif // __HEADER_H__
